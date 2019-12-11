bbc3e7657b532ffc6a163e3d6ef26c1818eb86dc
Edgaras Zavialovas <S00178556@mail.itsligo.ie>
2019-12-11 09:56:45 +0000

    Add project files.

---
+++ ExamS00178556/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamS00178556
+{
+    private static Random rng = new Random();
+    public enum Position { Goalkeeper, Defender, Midfielder, Forward}
+    public class Player
+
+    {
+
+        public string FName { get; set; }
+
+        public string Surname { get; set;}
+
+        public Position TypeOfPosition { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateTime.Now.DayOfYear < DateOfBirth.DayOfYear)
+                    age--;
+                return age;
+            }
+        }
+        
+    }
+}
